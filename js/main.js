0f3c25ff3c6ed07834e249885c9e8eb66642d90b
Sheel Shah <13870434+ShahSheel@users.noreply.github.com>
2020-08-15 19:16:43 +0100

    Removed Preloader

--- js/main.js
+++ js/main.js
@@ -451,7 +451,7 @@
     * ------------------------------------------------------ */
     (function ssInit() {
         
-        clPreloader();
+       // clPreloader();
         clMenuOnScrolldown();
         clOffCanvas();
         clPhotoswipe();
